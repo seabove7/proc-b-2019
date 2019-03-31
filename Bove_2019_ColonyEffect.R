fd522752b6d6309c860428c6026724c921e57f4c
Colleen Bove <45176386+seabove7@users.noreply.github.com>
2019-03-31 10:34:01 -0400

    Updated colony effect analysis with Bayesian model

--- Bove_2019_ColonyEffect.R
+++ Bove_2019_ColonyEffect.R
@@ -1,6 +1,6 @@
-########################################################################################################################
-############## Colony-level effect code for Bove et al. 2019 written by Colleen Bove and James Umbanhowar ##############
-########################################################################################################################
+######################################################################################################################
+################ Colony Effect code for Bove et al. 2019 written by Colleen Bove and James Umbanhowar ################
+######################################################################################################################
 
 ### Libraries required for this code
 library(ggplot2)
@@ -11,6 +11,8 @@ library(dplyr)
 library(data.table)
 library(stringr)
 library(cowplot)
+library(brms)
+library(ggridges)
 
 
 #################
@@ -18,7 +20,8 @@ library(cowplot)
 #################
 
 ### Read in data and manipulate dataframe
-coral <- read.csv("rate_22May17.csv")
+coral <- read.csv("rate05Feb19.csv")
+coral[1] <- NULL # this is an arbitrary column that can be removed
 coral[1] <- NULL # this is an arbitrary column that can be removed
 coral$tp <- as.factor(coral$tp) # set tp (time point) as a factor
 coral$species <- factor(coral$species, levels = c("S", "P", "A", "T")) # reorder species levels (S = S. siderea; P = P. strigosa; A = P. astreoides; U = U. tenuifolia)
@@ -30,7 +33,7 @@ coral$colony <- paste(coral$species, coral$colony, sep = "")
 coral$colony <- paste(coral$colony, coral$reefzone, sep = "_")
 
 # subset dataframe for just final T90 data (total)
-ceffect <- subset(coral, tp == "total")
+ceffect <- subset(coral, tp == "total") # & pco2 %in% c("pre", "cur", "eoc") & species %in% c("S","A"))
 
 # this function remove rows with any missing values
 completeFun <- function(data, desiredCols) {
@@ -41,106 +44,116 @@ ceffect <- completeFun(ceffect, "rate") # run function to remove any missing val
 
 
 #################
-## Correlation coefficients between colony intercept and slope
+## Bayesian Model for Colony Effect
 #################
 
-# mixed effect model run using lmer()
-colonymodel <- lmer(rate ~ species*(pco2+ftemp)+(1+species | tank)+(1 + (pco2+ftemp) | colony), data = ceffect)
+stanncmod <- brm(rate ~ species*(pco2+ftemp) + (1+ftemp+pco2| colony),family=gaussian(), data = ceffect,cores=4)
 
-ceffect$resid<-resid(colonymodel) # add model residuals to dataframe
+# examine the credible intervals
+summary(stanncmod) # show 95% credible
+summary(stanncmod,prob=.75) # show 75% credible ...
+ranef(stanncmod) # show all random effects
 
-### Extracting the bootstrapped correlation coefficients of colony from the model:
+# Make the three plots of the mean random effects against each other.
 
-bootnum = 15 #00 # set number of iterations (we used 1500) between 999 and 9999
-seed = 4 #seed to make results replicatable (our seed was 4)
+#make df of colony random effects
+colony <- data.frame(ranef(stanncmod)$colony[,1,])
+colony.error <- data.frame(ranef(stanncmod)$colony[,2,])
+colnames(colony.error)[1:5] <- c("base", "temp31", "cur", "eoc", "ext") # rename columns for error
+colony.error$species<-c(rep("A", times=12), rep("P", times=12),rep("S", times=12), rep("T", times=10)) # add a species column
+head(colony.error)
 
-out <- simulate(colonymodel,nsim=bootnum,seed=seed,re.form=NULL) # simulate your model set number of times in a dataframe (samples using random effects)
-boots <- apply(out,2,function(x) predict(lmer(x ~  species * (pco2 + ftemp) + (1 + species | tank) + (1 + (pco2 + ftemp) | colony), data = ceffect, REML=T),re.form=NA)) # applies the predict (does not use random effects) FUNCTION to the columns of the 'out' dataframe to ...
+both <- cbind(colony, colony.error)
+setDT(both, keep.rownames = TRUE)[]
+colnames(both)[1] <- c("colony")
+head(both)
 
-boot2 <- apply(out,2,function(x) as.data.frame(VarCorr(lmer(x ~  species * (pco2 + ftemp) + (1 + species | tank) + (1 + (pco2 + ftemp) | colony), data = ceffect, REML=T)))[6:9,5]) # Fit the same model to all the simulated data and extract the 4 correlations we want (correlations between colony intercept and slopes are rows 6-9 and column 5)
+#write.csv(both, "Bayesian_output19Feb19.csv")
+both <- read.csv("Bayesian_output19Feb19.csv", header=T)
 
-corrframe <- cbind(as.data.frame(VarCorr(colonymodel))[6:9,c(2,3,5)],t(apply(boot2,1,function(x) quantile(x, c(0.025, 0.975))))) # estimate the 95% confidence intervals for the 4 correlations
-colnames(corrframe)[4:5] <- c("lowerci2", "upperci2") # rename bounds of CI
+#Intercept vs. current
+cur.fig<- ggplot(data= both, aes(Intercept, pco2cur))+
+  geom_errorbarh(aes(xmin = Intercept-base, xmax = Intercept+base), colour="grey")+
+  geom_errorbar(aes(ymin = pco2cur-cur, ymax = pco2cur+cur), colour="grey")+
+  geom_point(size=2, aes(colour=species, shape=species))+
+  theme(panel.grid.major=element_blank(), panel.grid.minor=element_blank(), panel.background=element_blank())+ 
+  theme(axis.text.x=element_text(angle=45,hjust=1), legend.position="bottom")+
+  scale_colour_manual(labels=c("PAST","PSTR","SSID","UTEN"), values=c("#009E73", "#0072B2", "#D55E00", "#CC79A7"))+
+  scale_shape_manual(labels=c("PAST","PSTR","SSID","UTEN"), values=c(15,16,17,18))+
+  ylim(-0.7, 0.7)
 
-# combining the dataframes                            
-boots <- cbind(predict(colonymodel,re.form=NA), boots) #combine the predicted values with the simulated values into one dataframe 
-ceffect.a <- cbind(ceffect, predict(colonymodel,re.form=NA), as.data.frame(t(apply(boots, 1, function(x) quantile(x, c(0.025, 0.975)))))) # estimate the 95% confidence intervals for the prediction and the 95% bootstrap
-colnames(ceffect.a)[18:20] <- c("mean", "lowerci", "upperci") # rename mean and 95% CI bounds
-head(ceffect.a)
 
-## simple plot of mean correlation coefficients and 95% CI per stressor
-ggplot(corrframe, aes(x=var2, y=sdcor))+
-  geom_point()+
-  geom_errorbar(aes(ymin= lowerci2, ymax=upperci2))+
-  geom_hline(yintercept=0, linetype=2, colour="#999999")+
-  xlab("Treatment")+
-  ylab("Correlation coefficient")
+#Intercept vs eoc
+eoc.fig<- ggplot(data= both, aes(Intercept, pco2eoc))+
+  geom_errorbarh(aes(xmin = Intercept-base, xmax = Intercept+base), colour="grey")+
+  geom_errorbar(aes(ymin = pco2eoc-eoc, ymax = pco2eoc+eoc), colour="grey")+
+  geom_point(size=2, aes(colour=species, shape=species))+
+  theme(panel.grid.major=element_blank(), panel.grid.minor=element_blank(), panel.background=element_blank())+ 
+  theme(axis.text.x=element_text(angle=45,hjust=1), legend.position="bottom")+
+  scale_colour_manual(labels=c("PAST","PSTR","SSID","UTEN"), values=c("#009E73", "#0072B2", "#D55E00", "#CC79A7"))+
+  scale_shape_manual(labels=c("PAST","PSTR","SSID","UTEN"), values=c(15,16,17,18))+
+  ylim(-0.7, 0.7)
 
-write.csv(ceffect.a,file="colony_effect.csv") # save this dataframe as a CSV to keep bootstrapped mean and CI without having to rerun
+
+#Intercept vs extreme
+ext.fig<- ggplot(data= both, aes(Intercept, pco2ext))+
+  geom_errorbarh(aes(xmin = Intercept-base, xmax = Intercept+base), colour="grey")+
+  geom_errorbar(aes(ymin = pco2ext-ext, ymax = pco2ext+ext), colour="grey")+
+  theme(panel.grid.major=element_blank(), panel.grid.minor=element_blank(), panel.background=element_blank())+ 
+  theme(axis.text.x=element_text(angle=45,hjust=1), legend.position="bottom")+
+  scale_colour_manual(labels=c("PAST","PSTR","SSID","UTEN"), values=c("#009E73", "#0072B2", "#D55E00", "#CC79A7"))+
+  scale_shape_manual(labels=c("PAST","PSTR","SSID","UTEN"), values=c(15,16,17,18))+
+  geom_point(size=2, aes(colour=species, shape=species))+
+  ylim(-0.7, 0.7)
+
+
+#Intercept vs 31C
+temp.fig<- ggplot(data= both, aes(Intercept, ftemp31))+
+  geom_errorbarh(aes(xmin = Intercept-base, xmax = Intercept+base), colour="grey")+
+  geom_errorbar(aes(ymin = ftemp31-temp31, ymax = ftemp31+temp31), colour="grey")+
+  geom_point(size=2, aes(colour=species, shape=species))+
+  theme(panel.grid.major=element_blank(), panel.grid.minor=element_blank(), panel.background=element_blank())+ 
+  theme(axis.text.x=element_text(angle=45,hjust=1), legend.position="bottom")+
+  scale_colour_manual(labels=c("PAST","PSTR","SSID","UTEN"), values=c("#009E73", "#0072B2", "#D55E00", "#CC79A7"))+
+  scale_shape_manual(labels=c("PAST","PSTR","SSID","UTEN"), values=c(15,16,17,18))+
+  ylim(-0.7, 0.7)
+
+# use cowplot to plot the three desired figures
+REplot <- plot_grid(cur.fig, eoc.fig, temp.fig, labels = c("A", "B", "C"), align = 'h', ncol = 3)
+REplot
 
 
 #################
-## Plot random effects of colony
+## Plotting violin plots of the correlation coefficients
 #################
 
-## create the colony random effect dataframe
-ranef.colony<-ranef(colonymodel)$colony # create dataframe of the random effects of colony from the model
-setDT(ranef.colony, keep.rownames = TRUE)[] # convert row names to column
-ranef.colony$species<-c(rep("A", times=12), rep("P", times=12),rep("S", times=12), rep("T", times=10)) # add a species column
-colnames(ranef.colony)[1:6] <- c("colony","base", "cur", "eoc","ext","temp") # rename columns
+library(magrittr)
+library(dplyr)
+library(forcats)
+library(tidyr)
+library(modelr)
+library(tidybayes)
+library(ggplot2)
+library(ggstance)
+library(ggridges)
+library(cowplot)
+library(rstan)
+library(brms)
+library(ggrepel)
+theme_set(theme_tidybayes() + panel_border() + background_grid()) # set the theme
+
+# these help it run faster
+rstan_options(auto_write = TRUE)
+options(mc.cores = parallel::detectCores())
+
+# Make violin plot of the correlation coefficients from brms model with 75 and 95% credible intervals!
+stanncmod %>% 
+  spread_draws(b_Intercept, c(cor_colony__Intercept__pco2cur, cor_colony__Intercept__pco2eoc, cor_colony__Intercept__pco2ext, cor_colony__Intercept__ftemp31)) %>%
+  gather(treat, correlation, cor_colony__Intercept__ftemp31:cor_colony__Intercept__pco2ext) %>%
+  ggplot(aes(y= treat, x= correlation)) +
+  geom_vline(xintercept=0, linetype=2, colour="#999999") +
+  geom_halfeyeh(.width = c(0.75,0.95)) +
+  theme(panel.grid.major=element_blank(), panel.grid.minor=element_blank(), panel.background=element_blank())
 
 
-## plot random effect of colony on each stress treatment (current-day pCO2, end-of-century pCO2, extreme pCO2, 31C temperature) against 'base treatment' (pre-indusctrial pCO2 at 28C)
-
-theme_set(theme_classic()) # set the theme
-# plot of current-day pCO2
-cur.p<-
-  ggplot(ranef.colony, aes(x=base, y=cur, colour=species,shape=species))+
-  geom_point(size=2)+
-  scale_colour_manual(labels=c("PAST","PSTR","SSID","UTEN"), values=c("#009E73", "#0072B2", "#D55E00", "#CC79A7"))+
-  #theme(legend.position="none",axis.text.y=element_blank())+
-  ylab("colony random effect for
-  present-day pCO2 at 28C")+ # this spaciing allows ylab title to be two lines
-  xlab("colony random effect for
-  pre-industrial pCO2 at 28C")+ # this spaciing allows ylab title to be two lines
-  scale_shape_manual(labels=c("PAST","PSTR","SSID","UTEN"), values=c(15,16,17,18)) # set shape of points based on species
-
-# plot of end-of-century pCO2
-eoc.p<-ggplot(ranef.colony, aes(x=base, y=eoc, colour=species,shape=species))+
-  geom_point(size=2)+
-  scale_colour_manual(labels=c("PAST","PSTR","SSID","UTEN"), values=c("#009E73", "#0072B2", "#D55E00", "#CC79A7"))+
-  #theme(legend.position="none",axis.text.y=element_blank())+
-  ylab("colony random effect for
-  end-of-century pCO2 at 28C")+ # this spaciing allows ylab title to be two lines
-  xlab("colony random effect for
-  pre-industrial pCO2 at 28C")+ # this spaciing allows ylab title to be two lines
-  scale_shape_manual(labels=c("PAST","PSTR","SSID","UTEN"), values=c(15,16,17,18)) # set shape of points based on species
-
-# plot of extreme pCO2
-ext.p<-ggplot(ranef.colony, aes(x=base, y=ext, colour=species,shape=species))+
-  geom_point(size=2)+
-  scale_colour_manual(labels=c("PAST","PSTR","SSID","UTEN"), values=c("#009E73", "#0072B2", "#D55E00", "#CC79A7"))+
-  #theme(legend.position="none",axis.text.y=element_blank())+
-  ylab("colony random effect for
-  extreme pCO2 at 28C")+ # this spaciing allows ylab title to be two lines
-  xlab("colony random effect for
-  pre-industrial pCO2 at 28C")+ # this spaciing allows ylab title to be two lines
-  scale_shape_manual(labels=c("PAST","PSTR","SSID","UTEN"), values=c(15,16,17,18)) # set shape of points based on species
-
-# plot of 31C temperature
-temp.p<-ggplot(ranef.colony, aes(x=base, y=temp, colour=species,shape=species))+
-  geom_point(size=2)+
-  scale_colour_manual(labels=c("PAST","PSTR","SSID","UTEN"), values=c("#009E73", "#0072B2", "#D55E00", "#CC79A7"))+
-  #theme(legend.position="none",axis.text.y=element_blank())+
-  ylab("colony random effect for
-  pre-industrial pCO2 at 31C")+ # this spaciing allows ylab title to be two lines
-  xlab("colony random effect for
-  pre-industrial pCO2 at 28C")+ # this spaciing allows ylab title to be two lines
-  scale_shape_manual(labels=c("PAST","PSTR","SSID","UTEN"), values=c(15,16,17,18)) # set shape of points based on species
-
-
-## plot all four figures
-plot_grid(cur.p, eoc.p, ext.p, temp.p, ncol=2, nrow=2)
-
-## plot only current pCO2, EOC pCO2, and 31C as one figure like in publication
-plot_grid(cur.p, eoc.p, temp.p, ncol=3, nrow=1)
+  
